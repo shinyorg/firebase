c67bbe9f0f6f3b08f1d1519e9c48e1c34c7b4f1a
Allan Ritchie <allan.ritchie@gmail.com>
2025-09-17 12:54:22 -0400

    WIP

---
+++ Shiny.Push.FirebaseMessaging/FirebaseConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using System;
+#if ANDROID
+using Android.App;
+#endif
+
+namespace Shiny.Push;
+
+
+public record FirebaseConfiguration(
+    /// <summary>
+    /// If you have included a GoogleService-Info.plist (iOS) or google-services.json (Android)
+    /// </summary>
+    bool UseEmbeddedConfiguration = true,
+
+    string? AppId = null,
+    string? SenderId = null,
+    string? ProjectId = null,
+    string? ApiKey = null
+
+#if ANDROID
+    , NotificationChannel? DefaultChannel = null
+    , string? IntentAction = null
+#endif
+)
+{
+    public static FirebaseConfiguration Embedded { get; } = new(true);
+
+    public void AssertValid()
+    {
+        if (this.UseEmbeddedConfiguration)
+            return;
+
+        if (this.AppId == null)
+            throw new ArgumentNullException(nameof(this.AppId));
+
+        if (this.SenderId == null)
+            throw new ArgumentNullException(nameof(this.SenderId));
+
+        if (this.ApiKey == null)
+            throw new ArgumentNullException(nameof(this.ApiKey));
+
+        if (this.ProjectId == null)
+            throw new ArgumentNullException(nameof(this.ProjectId));
+    }
+}
---
+++ Shiny.Push.FirebaseMessaging/Platforms/iOS/FirebasePushProvider.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Shiny.Firebase.Analytics.iOS.Binding;
+using Shiny.Firebase.Messaging.iOS.Binding;
+using Foundation;
+
+namespace Shiny.Push;
+
+
+public class FirebasePushProvider(FirebaseConfiguration config) : NotifyPropertyChanged, IPushProvider, IPushTagSupport
+{
+
+    public async Task<string> Register(NSData nativeToken)
+    {
+        this.TryStartFirebase();
+        var fcmToken = await FirebaseMessaging.RegisterAsync(nativeToken);
+        if (fcmToken == null)
+             throw new InvalidOperationException("FCM Token is null");
+        
+        return fcmToken;
+    }
+
+
+    public Task UnRegister()
+    {
+        this.RegisteredTags = null;
+        return FirebaseMessaging.UnRegisterAsync();
+    }
+
+
+    string[]? registeredTags;
+    public string[]? RegisteredTags
+    {
+        get => this.registeredTags;
+        set => this.Set(ref this.registeredTags, value);
+    }
+
+
+    public async Task AddTag(string tag)
+    {
+        var tags = this.RegisteredTags?.ToList() ?? new List<string>(1);
+        tags.Add(tag);
+
+        await FirebaseMessaging.SubscribeAsync(tag).ConfigureAwait(false);
+        this.RegisteredTags = tags.ToArray();
+    }
+
+
+    public async Task RemoveTag(string tag)
+    {
+        await FirebaseMessaging
+            .UnSubscribeAsync(tag)
+            .ConfigureAwait(false);
+
+        if (this.RegisteredTags != null)
+        {
+            var tags = this.RegisteredTags.ToList();
+            if (tags.Remove(tag))
+                this.RegisteredTags = tags.ToArray();
+        }
+    }
+
+
+    public async Task ClearTags()
+    {
+        if (this.RegisteredTags != null)
+        {
+            foreach (var tag in this.RegisteredTags)
+            {
+                await FirebaseMessaging
+                    .UnSubscribeAsync(tag)
+                    .ConfigureAwait(false);
+            }
+        }
+        this.RegisteredTags = null;
+    }
+
+
+    public async Task SetTags(params string[]? tags)
+    {
+        await this.ClearTags().ConfigureAwait(false);
+        if (tags != null)
+        {
+            foreach (var tag in tags)
+                await this.AddTag(tag).ConfigureAwait(false);
+        }
+    }
+
+
+    protected virtual void TryStartFirebase()
+    {
+        if (!FirebaseApplication.IsConfigured)
+        {
+            if (config.UseEmbeddedConfiguration)
+            {
+                FirebaseApplication.AutoConfigure();
+                FirebaseMessaging.IsAutoInitEnabled = true;
+            }
+            else
+            {
+                FirebaseApplication.Configure(
+                    config.AppId!,
+                    config.SenderId!,
+                    config.ApiKey,
+                    config.ProjectId
+                );
+            }
+        
+            if (!FirebaseApplication.IsConfigured)
+                throw new InvalidOperationException("Firebase Application failed to configure - please check your settings");
+        }
+    }
+}
